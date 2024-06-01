ed6440f220bc62d50d4d86379768bb450b6aaed4
SUA GARCIA JHOAN SEBASTIAN <sebastiansua90@gmail.com>
2024-05-31 21:25:40 -0500

    Google SignUp/LogIn Implementation finished

--- app/src/main/java/com/udistrital/myintensehorario/Repository/UserRepository.kt
+++ app/src/main/java/com/udistrital/myintensehorario/Repository/UserRepository.kt
@@ -1,11 +1,15 @@
 package com.udistrital.myintensehorario.Repository
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.AuthResult
 import com.udistrital.myintensehorario.Model.User
 
 interface UserRepository {
 
-    fun singUp(user: User): Task<AuthResult>
+    fun signUp(user: User): Task<AuthResult>
     fun logIn(email: String, password: String): Task<AuthResult>
+    fun googleLogIn(credential: AuthCredential): Task<AuthResult>
+
 }
--- app/src/main/java/com/udistrital/myintensehorario/Service/UserService.kt
+++ app/src/main/java/com/udistrital/myintensehorario/Service/UserService.kt
@@ -2,20 +2,28 @@ package com.udistrital.myintensehorario.Service
 
 import android.widget.Toast
 import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.udistrital.myintensehorario.Model.User
 import com.udistrital.myintensehorario.Repository.UserRepository
 
 class UserService : UserRepository {
-    override fun singUp(user: User): Task<AuthResult> {
-        val db = Firebase.database
-        val ref = db.getReference("users")
+    private val db = Firebase.database
+    private val ref = db.getReference("users")
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
-        val authTask = FirebaseAuth.getInstance().createUserWithEmailAndPassword(user.email, user.password)
+
+    override fun signUp(user: User): Task<AuthResult> {
+
+
+        val authTask =
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(user.email, user.password)
         authTask.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val firebaseUser = task.result?.user
@@ -36,7 +44,28 @@ class UserService : UserRepository {
     }
 
     override fun logIn(email: String, password: String): Task<AuthResult> {
-        return FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    override fun googleLogIn(credential: AuthCredential): Task<AuthResult> {
+        val authTask = auth.signInWithCredential(credential)
+        authTask.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val firebaseUser = task.result?.user
+                val uid = firebaseUser?.uid
+                if (uid != null) {
+                    val userMap = mapOf(
+                        "uid" to uid,
+                        "name" to firebaseUser.displayName,
+                        "email" to firebaseUser.email,
+                    )
+                    ref.child(uid).setValue(userMap)
+                }
+            } else {
+                println("Error: ${task.exception?.message}")
+            }
+        }
+        return authTask
     }
 
 
